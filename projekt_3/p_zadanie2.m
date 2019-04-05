9168bebb71d05a0b243346e7118010a23273a29b
Kamil W <kamkam@onet.eu>
2019-04-05 17:44:05 +0200

    Fix p_zadanie2 so that the whole domain used (from -1 to 1)

--- projekt_3/p_zadanie2.m
+++ projekt_3/p_zadanie2.m
@@ -3,10 +3,10 @@ czas_sym = 200;
 
 
 u= ones(czas_sym, 1);
-y=zeros(czas_sym, 20);
+y=zeros(czas_sym, 21);
 figure(1);
-for i = 1:20
-    u=ones(czas_sym, 1)*(-1+i*0.1);
+for i = 1:21
+    u=ones(czas_sym, 1)*(-1.1+i*0.1);
     
     for k = 7:czas_sym
         y(k, i) = symulacja_obiektu3y(u(k-5),u(k-6),y(k-1, i),y(k-2, i));
@@ -18,8 +18,10 @@ for i = 1:20
 end
 hold off;
 
+ux = -1:0.1:1;
+
 figure(2);
-plot(y(czas_sym, :));
+plot(ux,y(czas_sym, :));
 
 % nazwa1 = sprintf('sprawko_dane/DMC_bez_zak/U__DMC_D=%g_N=%g_Nu=%g_L=%g_E=%g_.txt',D,N,Nu,lambda,wskaznikDMC);
 %     nazwa2 = sprintf('sprawko_dane/DMC_bez_zak/Y__DMC_D=%g_N=%g_Nu=%g_L=%g_E=%g_.txt',D,N,Nu,lambda,wskaznikDMC);
